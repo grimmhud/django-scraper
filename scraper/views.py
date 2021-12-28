0f9a99bbd6fba21221534c5315fba73293cce9dc
Hudson Oliveira <hudson.oliveira@easynvest.com.br>
2021-12-28 08:52:59 -0300

    add table from context v1

--- scraper/views.py
+++ scraper/views.py
@@ -1,18 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.template import Context
+from django.template.response import TemplateResponse
+from django.http import HttpResponseRedirect, JsonResponse
+from django.urls import reverse
+
+
 import json
 from .services.FileConverter import to_csv
 from .services.WebScraper import scrap_website
 
 
 def home_view(request):
+    context = {}
     if request.method == 'GET':
-        print('GET')
-        return render(request, 'home.html', {})
+        context['user'] = request.META['USER']
+        return render(request, 'home.html', context)
     if request.method == 'POST':
-        body = json.loads(request.body.decode('utf-8'))
-        data = scrap_website(body['website'], body['path'])
-        to_csv(data)
-        return render(request, 'home.html', {})
-    
-    print('None')
+        print('home POST')
+        extracted_data = scrap_website(request.POST['website'], request.POST['path'])
+        #to_csv(extracted_data)
+        print('home done')
+        context['extracted_data'] = extracted_data
+        return render(request, 'home.html', context)
     return render(request, 'home.html', {})
