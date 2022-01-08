9882e02c4b03f05b37135cbbaa912921c4ea3d36
Hudson Oliveira <hudson.oliveira@easynvest.com.br>
2022-01-08 16:47:29 -0300

    add export data selected on admin

--- scraper/admin.py
+++ scraper/admin.py
@@ -1,11 +1,27 @@
 from django.contrib import admin
 from .models import ScrapingSearch, ScrapingResult
+import csv
+from django.http import HttpResponse
+from .services.FileConverter import to_csv
+class ExportCsvMixin:
+    def export_as_csv(self, request, queryset):
+        meta = self.model._meta
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename={}.csv'.format(meta)
 
-class ScrapingResultAdmin(admin.ModelAdmin):
-    pass
+        for obj in queryset:
+            values = getattr(obj, 'values')
+            to_csv(response, values)
 
+        return response
+
+    export_as_csv.short_description = "Export Data Selected"
+
+@admin.register(ScrapingResult)
+class ScrapingResultAdmin(admin.ModelAdmin, ExportCsvMixin):
+    actions = ["export_as_csv"]
+
+
+@admin.register(ScrapingSearch)
 class ScrapingSearchAdmin(admin.ModelAdmin):
     pass
-
-admin.site.register(ScrapingResult, ScrapingResultAdmin)
-admin.site.register(ScrapingSearch, ScrapingSearchAdmin)
